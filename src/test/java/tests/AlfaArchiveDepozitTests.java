e7f62b6f29ab7eac537b94583f858ccdd5fc913a
Anton Babushking <napishi_antonu@mail.ru>
2020-12-16 17:19:50 +0300

    add test

---
+++ src/test/java/tests/AlfaArchiveDepozitTests.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class AlfaArchiveDepozitTests {
+
+    @Test
+    @DisplayName("Checking the number of deposits")
+    void AlfaArchiveDepozitTest() {
+        step("Open AlfaBank site", () -> {
+            open("https://alfabank.ru/");
+        });
+
+        step("Go to deposits", () -> {
+            $(byText("Вклады")).click();
+            $("body").shouldHave(text("Накопительные продукты"));
+        });
+
+        step("Go to the list of deposits", () -> {
+            $$(byText("Депозиты")).find(visible).parent().click();
+            $$(byText("Архивные счета и депозиты")).find(visible).parent().click();
+            $$(byText("Депозиты")).find(visible).parent().click();
+            $("body").shouldHave(text("Вклады"));
+        });
+
+        step("Сheck the number of deposits", () -> {
+            $$("div[data-widget-name='CatalogCard']").shouldHaveSize(5);
+        });
+    }
+}
